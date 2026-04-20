8dc48a1df3af6bdf090fb796a07cfe522361ea12
OLESIA <alesasarjevskaa@gmail.com>
2026-04-20 04:58:20 +0200

    test

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -3,3 +3,8 @@ declare module "*.jpg";
 declare module "*.jpeg";
 declare module "*.gif";
 declare module "*.webp";
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
